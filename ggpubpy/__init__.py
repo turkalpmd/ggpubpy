96b7daaba67eae97ab330eb7b6bce8f1142274af
turkalpmd <akbasli@ccf.org>
2025-06-20 12:36:51 -0400

    v0.2.1

--- ggpubpy/__init__.py
+++ ggpubpy/__init__.py
@@ -10,7 +10,7 @@ This project is directly inspired by R's ggpubr package.
 
 from typing import Any, List, Tuple
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Izzet Turkalp Akbasli"
 __email__ = "izzetakbasli@gmail.com"
 
